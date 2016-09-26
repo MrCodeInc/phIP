77f0e004646f878d02e1b9cc0108556b1db8767f
MrCode <MrCodeInc@users.noreply.github.com>
2016-09-25 21:41:38 -0400

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ function getRealIpAddr()
 $adresseip = getRealIpAddr();
 ?>
 <html><head>
-<title>Adresse IP: <?php echo $adresseip; ?></title>
+<title>Address IP: <?php echo $adresseip; ?></title>
 <style type="text/css">
 div { font-family:"Segoe UI","Lucida Grande","Calibri","Tahoma","Sans"; }
 div.ip { font-size:250%; }
@@ -28,8 +28,7 @@ div.desc { font-size:200%; }
 </style>
 </head>
 <body>
-<div><img src="csdccs-blanc.png" alt="logo CSDCCS" /></div>
-<p><div>Votre adresse IP: </div>
+<p><div>Your IP address is:</div>
 <div><?php echo $adresseip;?></div>
 </p>   
 </body>
